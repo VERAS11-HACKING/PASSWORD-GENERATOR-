2c575b02e488426f96a8e3b343ea2b4f6e2e5ec8
VERAS11-HACKING <sveraschutu@gmail.com>
2026-09-02 22:12:12 +0530

    Create password generator.sql

---
+++ password generator.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE password_generator;
+USE password_generator;
+CREATE TABLE passwords (
+id INT AUTO_INCREMENT PRIMARY KEY,
+password VARCHAR(255) NOT NULL
+);
+Convert this to MySQL file
